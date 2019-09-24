928aa88afe80e965091eaeda1e0567172390cbb7
Russ Garrett <russ@garrett.co.uk>
2019-09-24 15:46:40 +0100

    GS1 trait for SGTIN-198

--- src/epc/sgtin.rs
+++ src/epc/sgtin.rs
@@ -1,8 +1,8 @@
-use crate::epc::{EPC, EPCValue, GS1};
-use crate::epc::util::{read_string, uri_encode, zero_pad};
-use crate::general::ApplicationIdentifier;
 use crate::checksum::gs1_checksum;
+use crate::epc::util::{read_string, uri_encode, zero_pad};
+use crate::epc::{EPCValue, EPC, GS1};
 use crate::error::Result;
+use crate::general::ApplicationIdentifier;
 use bitreader::BitReader;
 
 #[derive(PartialEq, Debug)]
@@ -20,7 +20,10 @@ impl EPC for SGTIN96 {
     fn to_uri(&self) -> String {
         format!(
             "urn:epc:id:sgtin:{}.{}{}.{}",
-            zero_pad(self.company.to_string(), sgtin_company_digits(self.partition)),
+            zero_pad(
+                self.company.to_string(),
+                sgtin_company_digits(self.partition)
+            ),
             self.indicator.to_string(),
             zero_pad(self.item.to_string(), sgtin_item_digits(self.partition) - 1),
             self.serial
@@ -31,7 +34,10 @@ impl EPC for SGTIN96 {
         format!(
             "urn:epc:tag:sgtin-96:{}.{}.{}{}.{}",
             self.filter,
-            zero_pad(self.company.to_string(), sgtin_company_digits(self.partition)),
+            zero_pad(
+                self.company.to_string(),
+                sgtin_company_digits(self.partition)
+            ),
             self.indicator.to_string(),
             zero_pad(self.item.to_string(), sgtin_item_digits(self.partition) - 1),
             self.serial
@@ -45,9 +51,13 @@ impl EPC for SGTIN96 {
 
 impl GS1 for SGTIN96 {
     fn to_gs1(&self) -> String {
-        let element_string = format!("{}{}{}",
+        let element_string = format!(
+            "{}{}{}",
             self.indicator,
-            zero_pad(self.company.to_string(), sgtin_company_digits(self.partition)),
+            zero_pad(
+                self.company.to_string(),
+                sgtin_company_digits(self.partition)
+            ),
             zero_pad(self.item.to_string(), sgtin_item_digits(self.partition) - 1)
         );
         format!(
@@ -76,7 +86,10 @@ impl EPC for SGTIN198 {
     fn to_uri(&self) -> String {
         format!(
             "urn:epc:id:sgtin:{}.{}{}.{}",
-            zero_pad(self.company.to_string(), sgtin_company_digits(self.partition)),
+            zero_pad(
+                self.company.to_string(),
+                sgtin_company_digits(self.partition)
+            ),
             self.indicator.to_string(),
             zero_pad(self.item.to_string(), sgtin_item_digits(self.partition) - 1),
             uri_encode(self.serial.to_string())
@@ -87,7 +100,10 @@ impl EPC for SGTIN198 {
         format!(
             "urn:epc:tag:sgtin-198:{}.{}.{}{}.{}",
             self.filter,
-            zero_pad(self.company.to_string(), sgtin_company_digits(self.partition)),
+            zero_pad(
+                self.company.to_string(),
+                sgtin_company_digits(self.partition)
+            ),
             self.indicator.to_string(),
             zero_pad(self.item.to_string(), sgtin_item_digits(self.partition) - 1),
             uri_encode(self.serial.to_string())
@@ -96,6 +112,28 @@ impl EPC for SGTIN198 {
 
     fn get_value(&self) -> EPCValue {
         EPCValue::SGTIN198(self)
+    }
+}
+
+impl GS1 for SGTIN198 {
+    fn to_gs1(&self) -> String {
+        let element_string = format!(
+            "{}{}{}",
+            self.indicator,
+            zero_pad(
+                self.company.to_string(),
+                sgtin_company_digits(self.partition)
+            ),
+            zero_pad(self.item.to_string(), sgtin_item_digits(self.partition) - 1)
+        );
+        format!(
+            "({:0>2}) {}{} ({:0>2}) {}",
+            ApplicationIdentifier::GTIN as u16,
+            element_string,
+            gs1_checksum(&element_string),
+            ApplicationIdentifier::SerialNumber as u16,
+            self.serial
+        )
     }
 }
 
@@ -113,18 +151,18 @@ fn sgtin_item_digits(partition: u8) -> usize {
 // GS1 EPC TDS Table 14-2
 fn partition_bits(partition: u8) -> Result<(u8, u8)> {
     Ok(match partition {
-            0 => (40, 4),
-            1 => (37, 7),
-            2 => (34, 10),
-            3 => (30, 14),
-            4 => (27, 17),
-            5 => (24, 20),
-            6 => (20, 24),
-            _ => {
-                panic!(format!("Invalid partition value: {}", partition));
-                // TODO: error
-                //return Err("Invalid partition value");
-            }
+        0 => (40, 4),
+        1 => (37, 7),
+        2 => (34, 10),
+        3 => (30, 14),
+        4 => (27, 17),
+        5 => (24, 20),
+        6 => (20, 24),
+        _ => {
+            panic!(format!("Invalid partition value: {}", partition));
+            // TODO: error
+            //return Err("Invalid partition value");
+        }
     })
 }
 
@@ -132,11 +170,14 @@ fn extract_indicator(item: u64, partition: u8) -> Result<(u64, u8)> {
     // The first character of the correctly-padded item string is the indicator digit or must be
     // zero. I think.
     // This is not terribly well spelled out in the GS1 EPC spec.
+    //
+    // TODO: error handling could be improved, but in practice most of these errors are probably
+    // unreachable.
     let item_str = zero_pad(item.to_string(), sgtin_item_digits(partition));
     let mut item_str_iterator = item_str.chars();
     let indicator = item_str_iterator.next().unwrap().to_digit(10).unwrap() as u8;
     let item = item_str_iterator.collect::<String>().parse::<u64>()?;
-    return Ok((item, indicator))
+    return Ok((item, indicator));
 }
 
 // GS1 EPC TDC Section 14.5.1
@@ -152,14 +193,13 @@ pub(super) fn decode_sgtin96(data: &[u8]) -> Result<Box<dyn EPC>> {
     let serial = reader.read_u64(38)?;
 
     Ok(Box::new(SGTIN96 {
-            filter: filter,
-            partition: partition,
-            company: company,
-            item: item,
-            indicator: indicator,
-            serial: serial,
-        }),
-    )
+        filter: filter,
+        partition: partition,
+        company: company,
+        item: item,
+        indicator: indicator,
+        serial: serial,
+    }))
 }
 
 // GS1 EPC TDC Section 14.5.1.2
@@ -175,12 +215,11 @@ pub(super) fn decode_sgtin198(data: &[u8]) -> Result<Box<dyn EPC>> {
     let serial = read_string(reader, 140)?;
 
     Ok(Box::new(SGTIN198 {
-            filter: filter,
-            partition: partition,
-            company: company,
-            item: item,
-            indicator: indicator,
-            serial: serial,
-        }),
-    )
+        filter: filter,
+        partition: partition,
+        company: company,
+        item: item,
+        indicator: indicator,
+        serial: serial,
+    }))
 }
